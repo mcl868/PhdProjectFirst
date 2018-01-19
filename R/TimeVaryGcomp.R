3f4eb569625b74632e4e3c6ecb46902112bebef6
Thomas Maltesen <thma@sund.ku.dk>
2018-01-19 13:18:24 +0100

    Update TimeVaryGcomp.R

--- R/TimeVaryGcomp.R
+++ R/TimeVaryGcomp.R
@@ -1,14 +1,21 @@
-TimeVaryGcomp<-function(model, cond, data, family=gaussian()){
-  new.data<-data
+TimeVaryGcomp<-function(model, cond, data, missingObs=FALSE, family=gaussian()){
+  if(missingObs){
+    data$Nmis<-eval(parse(text=paste0("is.na(data$",colnames(data),")",collapse = "+")))
+    DataMis<-data[data$Nmis<=missingObs,]
+    fulldata<-data[eval(parse(text=paste0("!is.na(data$",colnames(data),")",collapse = " & "))),]
+    fulldata$Nmis<-NULL
+  } else {
+    fulldata<-DataMis<-data[eval(parse(text=paste0("!is.na(data$",colnames(data),")",collapse = " & "))),]
+  }
 
   result<-list(model=model)
-  result$data<-data
+  result$data<-fulldata
   
   for(i in 1:length(cond)){
-    tempvalues<-predict(glm(cond[[i]],data = data),type="response",newdata=new.data)
+    tempvalues<-predict(glm(cond[[i]],data = fulldata),type="response",newdata=DataMis)
     eval(parse(text = paste0("new.data$",all.vars(cond[[i]])[1],"<-tempvalues")))}
 
-  betahat<-matrix(glm(model,data = new.data)$coefficients[-1])
+  betahat<-matrix(glm(model,data = DataMis)$coefficients[-1])
   colnames(betahat)<-"Estimat"
   rownames(betahat)<-all.vars(model)[-1]
   result$betahat<-betahat
