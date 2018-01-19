332e903e760f82b6ae006fae5ce977f8378c4632
Thomas Maltesen <thma@sund.ku.dk>
2018-01-19 16:16:59 +0100

    Update print.Gestimation.R

--- R/print.Gestimation.R
+++ R/print.Gestimation.R
@@ -1,11 +1,17 @@
 print.Gestimation<-function(x, digits=4, ...){
   object<-x
   rm(x)
-  if(inherits(object,"Gestimation")){
+  if(inherits(object,"Gcomputation")){
     cat("\n")
-    cat("Model:\n")
-    print(object$model)
-    cat("\n")
-    print(object$thetahat)
-    }
+    cat(paste0("Call: TimeVaryGcomp(model = ",paste(object$model)[2]," ",paste(object$model)[1]," ",paste(object$model)[3], ","),"\n")
+    for(i in 1:length(object$treatment))
+      cat(paste0(if(i==1)"                    cond = c(" else "                             ",
+                 paste(object$treatment[[i]])[2]," ",
+                 paste(object$treatment[[i]])[1]," ",
+                 paste(object$treatment[[i]])[3],if(i==length(object$cond))(")," )else ","),
+          "\n")
+    if(object$missingObs)cat(paste0("                    missingObs = ",object$missingObs,","),"\n")
+    cat(paste0("                    data = ",2,")\n\n"))
+    print(round(object$coefficients,digits))
+  }
 }
