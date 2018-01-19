124be5d6b287b336f862aa995011b11378e6098d
Thomas Maltesen <thma@sund.ku.dk>
2018-01-19 10:35:39 +0100

    Nice output

--- R/TimeVaryGestV2.R
+++ R/TimeVaryGestV2.R
@@ -3,17 +3,21 @@ TimeVaryGestV2<-function(model, treatment, data, missingObs=FALSE, family=gaussi
     data$Nmis<-eval(parse(text=paste0("is.na(data$",colnames(data),")",collapse = "+")))
     DataMis<-data[data$Nmis<=missingObs,]
     fulldata<-data[eval(parse(text=paste0("!is.na(data$",colnames(data),")",collapse = " & "))),]
+    fulldata$Nmis<-NULL
   } else {
-    fulldata<-data[eval(parse(text=paste0("!is.na(data$",colnames(data),")",collapse = " & "))),]
-    DataMis<-data[eval(parse(text=paste0("!is.na(data$",colnames(data),")",collapse = " & "))),]
+    fulldata<-DataMis<-data[eval(parse(text=paste0("!is.na(data$",colnames(data),")",collapse = " & "))),]
   }
-  
+ 
   Ahat<-lapply(1:length(treatment),function(i)predict(glm(treatment[[i]],data=DataMis,family=family),type = "response",newdata=fulldata))
   A<-lapply(1:length(treatment),function(i)eval(parse(text=paste0("fulldata$", all.vars(formula(treatment[[i]]))[1]))))
   TreatRes<-lapply(1:length(treatment),function(i)A[[i]]-Ahat[[i]])
   
   Ntreat<-length(treatment)
   B<-diag(Ntreat)
+  
+  
+  result<-list(model=model)
+  result$data<-fulldata
   
   SumMatrix<-matrix(0,Ntreat,Ntreat)
   for(j in 1:nrow(fulldata)){
@@ -27,14 +31,23 @@ TimeVaryGestV2<-function(model, treatment, data, missingObs=FALSE, family=gaussi
     }
     SumMatrix<-SumMatrix+TemP
   }
-  
+  result$SumMatrix<-SumMatrix
+
   SumMatrixResponse<-matrix(0,Ntreat,1)
   for(j in 1:nrow(fulldata))SumMatrixResponse<-SumMatrixResponse+
     eval(parse(text=
                  paste0("TreatRes[[",c(1:Ntreat),"]][",j,"]*B[,",c(1:Ntreat),",drop=FALSE]*fulldata$",
                         all.vars(model)[1],"[",j,"]",collapse="+")))
+  result$SumMatrixResponse<-SumMatrixResponse
+  
   thetahat<-solve(SumMatrix)%*%SumMatrixResponse
   colnames(thetahat)<-"Estimat"
   rownames(thetahat)<-all.vars(model)[-1]
-  return(thetahat)
+
+  result$thetahat<-thetahat
+  
+  attr(result,"class")<-"Gestimation"
+  out<-structure(result, class = "Gestimation")
+  
+  return(out)
 }
