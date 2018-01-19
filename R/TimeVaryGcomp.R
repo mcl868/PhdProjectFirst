fc0c467f10307a54919bb50c4822e5a99331da0f
Thomas Maltesen <thma@sund.ku.dk>
2018-01-19 16:31:28 +0100

    Update TimeVaryGcomp.R

--- R/TimeVaryGcomp.R
+++ R/TimeVaryGcomp.R
@@ -12,6 +12,7 @@ TimeVaryGcomp<-function(model, cond, data, missingObs = FALSE, family = gaussian
   result$cond<-cond
   result$missingObs<-missingObs
   result$data<-fulldata
+  result$Namedata<-deparse(substitute(data))
   
   for(i in 1:length(cond)){
     tempvalues<-predict(glm(cond[[i]],data = DataMis),type="response",newdata=fulldata)
