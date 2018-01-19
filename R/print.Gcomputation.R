1a68a2027caac3b55a2bd8d4da42af09d45ae319
Thomas Maltesen <thma@sund.ku.dk>
2018-01-19 15:30:48 +0100

    Update print.Gcomputation.R

--- R/print.Gcomputation.R
+++ R/print.Gcomputation.R
@@ -1,2 +1,18 @@
-print.Gcomputation1<-function(x, digits=4, ...){
+print.Gcomputation<-function(x, digits=4, ...){
+  object<-x
+  rm(x)
+  if(inherits(object,"Gcomputation")){
+    cat("\n")
+    cat(paste0("Call: TimeVaryGcomp(model = ",paste(object$model)[2]," ",paste(object$model)[1]," ",paste(object$model)[3], ","),"\n")
+    for(i in 1:length(object$cond))
+      cat(paste0(if(i==1)"                    cond = c(" else "                             ",
+                 paste(object$cond[[i]])[2]," ",
+                 paste(object$cond[[i]])[1]," ",
+                 paste(object$cond[[i]])[3],if(i==length(object$cond))(")," )else ","),
+          "\n")
+    if(object$missingObs)cat(paste0("                    missingObs = ",object$missingObs,")"),"\n")
+    cat(paste0("                    data = ",2,")\n"))
+    cat("\n")
+    print(round(object$coefficients,digits))
+  }
 }
