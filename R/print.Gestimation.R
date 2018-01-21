159308261861e98cccf3814bfb939e9e7ed11da9
Thomas Maltesen <thomasmaltesen@thomasmesensMBP.lan>
2018-01-21 17:04:09 +0100

    Update

--- R/print.Gestimation.R
+++ R/print.Gestimation.R
@@ -5,7 +5,7 @@ print.Gestimation<-function(x, digits=4, ...){
     cat("\n")
     cat(paste0("Call: TimeVaryGest(model = ",paste(object$model)[2]," ",paste(object$model)[1]," ",paste(object$model)[3], ","),"\n")
     for(i in 1:length(object$cond))
-      cat(paste0(if(i==1)"                    cond = c(" else "                             ",
+      cat(paste0(if(i==1)"                   cond = c(" else "                            ",
                  paste(object$cond[[i]])[2]," ",
                  paste(object$cond[[i]])[1]," ",
                  paste(object$cond[[i]])[3],if(i==length(object$cond))(")," )else ","),
