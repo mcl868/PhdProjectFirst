0334f7cacd2792624946e9f1c9c156ad631226db
Thomas Maltesen <thma@sund.ku.dk>
2018-01-26 08:35:18 +0100

    Update bootstrapTimeVary.R

--- R/bootstrapTimeVary.R
+++ R/bootstrapTimeVary.R
@@ -2,22 +2,19 @@ bootstrapTimeVary<-function(x,n=1000){
   object<-x
   rm(x)
   if(inherits(object,"Gcomputation") | inherits(object,"Gestimation")){
-    object$data
-    object$Estdata
-    object$NBallObs
-    object$NBestObs
-    boot<-list()
-    for(ii in 1:n){
-      print(ii)
-      booo<-sample(c(1:object$NBestObs),object$NBestObs,replace=TRUE)
+    booo<-lapply(1:n,function(i)sample(c(1:object$NBestObs),object$NBestObs,replace=TRUE))
       if(inherits(object,"Gcomputation")){
-      boot[[ii]]<-TimeVaryGcomp(model=object$model,cond=object$cond,missingObs=object$missingObs,data=object$Estdata[booo,])$coefficients
+        boot<-lapply(1:n,function(i)
+          TimeVaryGcomp(model=object$model,cond=object$cond,
+                        missingObs=object$missingObs,
+                        data=object$Estdata[booo[[i]],])$coefficients)
       }
       if(inherits(object,"Gestimation")){
-      boot[[ii]]<-TimeVaryGest(model=object$model,cond=object$cond,missingObs=object$missingObs,data=object$Estdata[booo,])$coefficients
+        boot<-lapply(1:n,function(i)
+          TimeVaryGest(model=object$model,cond=object$cond,
+                        missingObs=object$missingObs,
+                        data=object$Estdata[booo[[i]],])$coefficients)
       }
     }
-  }
-  return(boot)
+  return(listVar(boot))
 }
-
