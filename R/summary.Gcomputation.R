4fa22a8f6a5c48cd6a3ceb96a7e811ce32180418
Thomas Maltesen <thma@sund.ku.dk>
2018-01-26 12:30:10 +0100

    Create summary.Gcomputation.R

---
+++ R/summary.Gcomputation.R
@@ -0,0 +1,19 @@
+summary.Gcomputation<-function(x, n=100, ...){
+  object<-x
+  rm(x)
+  if(inherits(object,"Gcomputation")){
+    xboot<-bootstrapTimeVary(object, n)
+    NamEst<-colnames(xboot)
+    EstMatrix<-matrix(NA,nrow=length(NamEst),ncol=4)
+    for(i in 1:length(NamEst))
+      EstMatrix[i,]<-
+      cbind(object$coefficients[,NamEst[i]],
+            sqrt(xboot[,NamEst[i]]),
+            object$coefficients[,NamEst[i]]+qnorm(0.025)*sqrt(xboot[,NamEst[i]]),
+            object$coefficients[,NamEst[i]]+qnorm(0.975)*sqrt(xboot[,NamEst[i]]))
+  }
+  result<-list(Est=EstMatrix)
+  attr(result, "class") <- "SumGcomp"
+  out <- structure(result, class = "SumGcomp")
+  return(out)
+}
